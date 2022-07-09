7c9bd726c4a35c8eac9a046ead8a70eb55611944
jaewoogwak <jaewoo010207@gmail.com>
2022-07-10 00:12:41 +0900

    Counter for Vanila-Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import { createStore } from "redux";
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+const ADD = "ADD";
+const MINUS = "MINUS";
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+const onChange = () => {
+  console.log("There was something change in store");
+  number.innerText = countStore.getState();
+};
+const countStore = createStore(countModifier);
+countStore.subscribe(onChange);
+console.log(countStore);
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
